aa1d4b4ee4eac692418b0d2e9914cbc681b6f974
Trevor Stanhope <tpstanhope@gmail.com>
2015-02-21 04:03:01 -0500

    just in case

--- nico-usb.py
+++ nico-usb.py
@@ -5,132 +5,145 @@ Python Interface for Nico USB HID sensors
 # Modules
 import usb.core
 import usb.util
-import ConfigParser 
-import shlex
-import subprocess
-import logging
 
 # Pretty Print
 def pretty_print(task, msg):
     print("%s\t%s" % (task, msg))
 
-# Config Loader
-def load_config():
-    dict={}
-    config = ConfigParser.RawConfigParser()
-    config.read('/codes/remote/remote.cfg')
-    dict['vendor']=config.getint('Settings','idVendor')
-    dict['product']=config.getint('Settings','idProduct')
-    dict['interface']=config.getint('Settings', 'interface')
-    r=config.options('Key Mappings')
-    for item in r:
-        if config.get('Key Mappings',item)!='': 
-            dict[item]=config.get('Key Mappings',item)
-            print config.get('Key Mappings',item)
-    return dict
-
 # PyUS Function #! TODO Make into class
-def pyus():
+class NicoUSB:
 
-    try:
-        pretty_print('WARNING', 'Creating udev device')
-        config = { 'product' : 8468, 'vendor' : '10c4', 'interface' : 0, 'manufacturer':'SLAB', 'timeout':1000}
-        all_devs = usb.core.find('10c4:8468')
-        for dev in all_devs:
-            pretty_print('DEVICE', '%s %s' % (dev.product, dev.manufacturer))
-            if dev.manufacturer == config['manufacturer']:
-                break
-    except Exception as e:
-        pretty_print('ERR 002', str(e))
-        
-    try:
-        pretty_print('WARNING', 'Getting udev config')
-        cfg = dev.get_active_configuration()
-        interface_number = cfg[(0,0)].bInterfaceNumber
-        pretty_print('WARNING', 'Interface #%d' % interface_number)
-    except Exception as e:
-        pretty_print('ERR 001', str(e))
-        
-    try:
-        pretty_print('WARNING', 'Getting alternate interface setting')
-        alternate_setting = usb.control.get_interface(dev, interface_number)
-    except usb.core.USBError as e:
-        pretty_print('ERR 100', str(e))
-    except Exception as e:
-        pretty_print('ERR 101', str(e))
-        
-    try:
-        pretty_print('WARNING', 'Finding descriptor of interface')
-        intf = usb.util.find_descriptor(cfg)
-    except Exception as e:
-        pretty_print('ERR 500', str(e))
-        
-    try:
-        pretty_print('WARNING', 'Finding EP In descriptor')
-        ep_in = usb.util.find_descriptor(
-            intf,
-            custom_match = \
-            lambda e: \
-                usb.util.endpoint_direction(e.bEndpointAddress) == \
-                usb.util.ENDPOINT_IN
-        )
-        assert ep_in is not None
-    except Exception as e:
-        pretty_print('ERR 201', 'Failed to set EP In')
-        
-    try:
-        pretty_print('WARNING', 'Finding EP Out descriptor')
-        ep_out = usb.util.find_descriptor(
-            intf,
-            custom_match = \
-            lambda e: \
-                usb.util.endpoint_direction(e.bEndpointAddress) == \
-                usb.util.ENDPOINT_OUT
-        )
-        assert ep_out is not None
-    except Exception as e:
-        pretty_print('ERR 201', 'Failed to set EP Out')
+    def __init__(self):
     
-    try:
-        interface = config['interface']
-        if dev is None:
-            raise ValueError('Device not found')
-        elif dev.is_kernel_driver_active(interface) is True:
-            pretty_print('WARNING', 'Attaching kernel driver')
-            dev.detach_kernel_driver(interface) # We need to detach kernel driver
-        else:
-            raise ValueError('Kernel driver inactive!')
-    except Exception as e:
-        pretty_print('ERR 301', str(e))
-    
-    try:
-        # Set the active configuration. With no arguments, the first will be the active one
-        pretty_print('WARNING', 'Setting configuration')
-        dev.set_configuration()
-    except Exception as e:
-        pretty_print('ERR 302', str(e))
- 
-    while True:
+        # Initialize Device Connection
         try:
-            dev.write(ep_in.bEndpointAddress, str(0x83), config['timeout'])
+            pretty_print('WARNING', 'Creating udev device')
+            config = {
+                'product' : 0x8468,
+                'vendor' : 0x10c4,
+                'interface' : 0,
+                'manufacturer' : 'SLAB',
+                'timeout' : 1000
+                }
+            self.config = config
+            self.dev = None
+            all_devs = usb.core.find() # Finding UDEV Devices
+            for d in all_devs:
+                try:
+                    pretty_print('DEVICE', '%s %s' % (d.product, d.manufacturer))
+                    if d.manufacturer == config['manufacturer']:
+                        self.dev = d
+                except Exception as e:
+                    pass
+            if self.dev is None: 
+                pretty_print('ERR 000', 'DEVICE NOT FOUND')
+                return
+            else:
+                pretty_print('WARNING', 'DEVICE OKAY')
+        except Exception as e:
+            pretty_print('ERR 002', str(e))
+        
+        if self.dev:
+            # Get the Active Configuration
+            try:
+                pretty_print('WARNING', 'Getting udev config')
+                self.cfg = self.dev.get_active_configuration()
+                interface_number = self.cfg[(0,0)].bInterfaceNumber
+                pretty_print('WARNING', 'Interface #%d' % interface_number)
+            except Exception as e:
+                pretty_print('ERR 001', str(e))
+    
+            # Alternate Settings
+            try:
+                pretty_print('WARNING', 'Getting alternate interface setting')
+                alternate_setting = usb.control.get_interface(self.dev, interface_number)
+            except usb.core.USBError as e:
+                pretty_print('ERR 100', str(e))
+            except Exception as e:
+                pretty_print('ERR 101', str(e))
+    
+            # Interface
+            try:
+                pretty_print('WARNING', 'Finding descriptor of interface')
+                intf = usb.util.find_descriptor(self.cfg)
+            except Exception as e:
+                pretty_print('ERR 500', str(e))
+    
+            # Endpoint In
+            try:
+                pretty_print('WARNING', 'Finding EP In descriptor')
+                self.ep_in = usb.util.find_descriptor(
+                    intf,
+                    custom_match = \
+                    lambda e: \
+                        usb.util.endpoint_direction(e.bEndpointAddress) == \
+                        usb.util.ENDPOINT_IN
+                )
+                assert self.ep_in is not None
+            except Exception as e:
+                pretty_print('ERR 201', 'Failed to set EP In')
+    
+            # Endpoint Out
+            try:
+                pretty_print('WARNING', 'Finding EP Out descriptor')
+                self.ep_out = usb.util.find_descriptor(
+                    intf,
+                    custom_match = \
+                    lambda e: \
+                        usb.util.endpoint_direction(e.bEndpointAddress) == \
+                        usb.util.ENDPOINT_OUT
+                )
+                assert self.ep_out is not None
+            except Exception as e:
+                pretty_print('ERR 201', 'Failed to set EP Out')
+
+            # Kernel Driver
+            try:
+                interface = self.config['interface']
+                if self.dev is None:
+                    raise ValueError('Device not found')
+                elif self.dev.is_kernel_driver_active(interface) is True:
+                    pretty_print('WARNING', 'Attaching kernel driver')
+                    self.dev.detach_kernel_driver(interface) # We need to detach kernel driver
+                else:
+                    raise ValueError('Kernel driver inactive!')
+            except Exception as e:
+                pretty_print('ERR 301', str(e))
+    
+            # Set the active configuration. With no arguments, the first will be the active one
+            try:
+                pretty_print('WARNING', 'Setting configuration')
+                self.dev.set_configuration()
+            except Exception as e:
+                pretty_print('ERR 302', str(e))
+
+    # Write --> Read Loop
+    def query_usb(self, cmd_byte = str(0x83)):
+        try:
+            pretty_print('WARNING', 'Writing to USB')
+            self.dev.write(self.ep_out.bEndpointAddress, cmd_byte, self.config['timeout'])
         except Exception as e:
             pretty_print('ERR 401', str(e))
+            return
             
         try:
-            data = dev.read(ep_in.bEndpointAddress, ep_in.wMaxPacketSize*2, 1000)
+            pretty_print('WARNING', 'Reading from USB')
+            data = dev.read(self.ep_in.bEndpointAddress, self.ep_in.wMaxPacketSize*2, self.config['timeout'])
             data = data.tolist()
-            print data
             key = join_int(data)
             print "Key is " , key
-            try:
-                args = shlex.split(dict[diction[key]])
-                print args
-                p = subprocess.Popen(args, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
-                print "Pressed key is ", diction[key]
-            except Exception as e:
-                pretty_print('ERR 403', str(e))
+            return data
+        except AttributeError as e:
+            pretty_print('ERR 403', str(e))
+            return
         except usb.core.USBError as e:
             pretty_print('ERR 402', str(e))
+            return
 
 if __name__ == '__main__':
-    pyus()
+    root = NicoUSB()
+    while True:
+        try:
+            print root.query_usb()
+        except KeyboardInterrupt:
+            break
